e691ac9f6f2d896fdb535b748f516f15d19571de
Gabrielgamas <gamas.enge@hotmail.com>
2025-07-30 12:48:42 -0300

    validando testes

--- src/products.test.ts
+++ src/products.test.ts
@@ -4,6 +4,7 @@ import { app } from "./app";
 describe("products", () => {
   it("should list products", async () => {
     const response = await request(app).get("/products");
-    console.log(response.body);
+
+    expect(response.statusCode).toBe(200);
   });
 });
