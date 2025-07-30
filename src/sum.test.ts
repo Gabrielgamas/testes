bce0e72bd523125c79e063bd4d5a5b11377a8478
Gabrielgamas <gamas.enge@hotmail.com>
2025-07-30 12:26:18 -0300

    usando beforeAll

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,7 +1,21 @@
 import { sum } from "./server";
 
-test("sum of 3 + 7 must be 10", () => {
-  const result = sum(3, 7);
+describe("sum", () => {
+  let sumResult: number;
 
-  expect(result).toBe(10);
+  beforeAll(() => {
+    sumResult = 10;
+  });
+
+  it("sum of 3 + 7 must be 10", () => {
+    const result = sum(3, 7);
+
+    expect(result).toBe(sumResult);
+  });
+
+  test("sum of 2 + 2 must be 4", () => {
+    const result = sum(2, 2);
+
+    expect(result).toBe(4);
+  });
 });
